c589ca14081c98773ad9d8149a8684078b778f69
Chris Skinner <chris.skinner@novia-financial.co.uk>
2019-05-08 18:49:15 +0100

    simplify MVC

---
+++ Reactec.Web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="HomeController.cs" company="Reactec Practical">
+// Copyright (c) Reactec Practical. All rights reserved.
+// </copyright>
+
+namespace Reactec.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Home controller
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Get Index.
+        /// </summary>
+        /// <returns>IActionResult.</returns>
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
--- Reactec.Web/Startup.cs
+++ Reactec.Web/Startup.cs
@@ -74,7 +74,12 @@ namespace Reactec.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
