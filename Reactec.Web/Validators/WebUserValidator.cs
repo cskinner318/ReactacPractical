82c44cee49c33b29fbcf3dc76365b0439b944924
Chris Skinner <chris.skinner@novia-financial.co.uk>
2019-05-09 05:52:55 +0100

    validation fixes

--- Reactec.Web/Validators/WebUserValidator.cs
+++ Reactec.Web/Validators/WebUserValidator.cs
@@ -19,15 +19,13 @@ namespace Reactec.Web.Validators
         public WebUserValidator()
         {
             this.RuleFor(x => x.DateOfBirth)
-                .NotNull().WithMessage("Date of birth is required")
+                .NotEmpty().WithMessage("Date of birth is required")
                 .LessThan(DateTime.Now.AddYears(-18))
                 .WithMessage("User must be at least 18 years old");
             this.RuleFor(x => x.Name)
-                .NotNull().WithMessage("Name is required")
                 .NotEmpty().WithMessage("Name is required");
             this.RuleFor(x => x.EmailAddress)
                 .NotEmpty().WithMessage("Email address is required")
-                .NotNull().WithMessage("Email address is required")
                 .EmailAddress().WithMessage("Valid email address is required");
         }
     }
